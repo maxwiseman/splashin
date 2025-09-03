296652d60ee047b27b0f6d51f9ae524f064765a1
maxwiseman <max@maxwiseman.io>
2025-09-02 21:56:51 -0400

    fix: fix logging issue

--- server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { eventHandler, getHeaders, readBody } from "h3";
-
-// Learn more: https://nitro.build/guide/routing
-export default eventHandler(async (event) => {
-  const body = await readBody(event);
-  const headers = getHeaders(event);
-  console.log("body", body);
-  console.log("headers", headers);
-});
