810767c574684965ef341ae7315f1de7316940d0
maxwiseman <max@maxwiseman.io>
2025-09-02 21:10:36 -0400

    chore: add logging

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,11 +1,9 @@
-import { eventHandler } from "h3"
+import { eventHandler, getHeaders, readBody } from "h3";
 
 // Learn more: https://nitro.build/guide/routing
-export default eventHandler((event) => {
-  return `
-      <meta charset="utf-8">
-      <h1>This is your brand new Nitro project 🚀 </h1>
-      <p>Get started by editing the <code>server/routes/index.ts</code> file.</p>
-      <p>Learn more from 📖 <a href="https://nitro.build/guide" target="_blank">Nitro Documentation</a></p>
-    `;
+export default eventHandler(async (event) => {
+  const body = await readBody(event);
+  const headers = getHeaders(event);
+  console.log("body", body);
+  console.log("headers", headers);
 });
